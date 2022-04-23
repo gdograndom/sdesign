a39e2b332a9afa59c60a9b38085bbaca35ec19bf
gdograndom <gavin@mcgeeclan.org>
2022-04-23 15:19:19 -0600

    upper limit text, rm 1 more space

--- controlGUIADC.py
+++ controlGUIADC.py
@@ -31,7 +31,7 @@ col_1 = [  [sg.Text('Drive Mode', size=(29,1), font='Helvetica 20', justificatio
            [sg.Button('', image_data=boost_drive_base64, button_color=sg.TRANSPARENT_BUTTON, border_width=5, image_subsample=1, size=(4, 4), key='boost'), sg.Button('', image_data=regen_braking_base64, button_color=sg.TRANSPARENT_BUTTON, border_width=5, image_subsample=1, size=(4, 4), key='regen'), sg.Text('', size=(3,1), font='Helvetica 30')] ]
 
 col_2 = [  [sg.Text('', size=(13,1), font='Helvetica 20'), sg.Button('', image_data=red_x_base64, button_color=sg.TRANSPARENT_BUTTON, border_width=0, image_subsample=8, key='Exit')],
-		   [sg.Text('        3300 PSI', size=(13,1), font='Helvetica 13')],
+		   [sg.Text('       3300 PSI', size=(13,1), font='Helvetica 13')],
            [sg.Text('', size=(2,1), font='Helvetica 20'), sg.ProgressBar(1, orientation='w', size=(32, 60), key='pressurePROG')],
            [sg.Text('', size=(30,1), font='Helvetica 25', key='pressureTXT')] ]
 
